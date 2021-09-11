4f80c4c9888aad99edbcdaf34718f26b52c9bbdd
Maciej Rapacki <mac.rapacki@gmail.com>
2021-09-11 18:34:40 +0100

    feat: style faq section

--- src/components/organisms/FaqSection.jsx
+++ src/components/organisms/FaqSection.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core";
+
+import { Box } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  section: {
+    width: "100%",
+    backgroundColor: "#f9f9f9",
+  },
+});
+
 const FaqSection = () => {
-  return <section style={{ width: "100%" }}></section>;
+  const classes = useStyles();
+
+  return (
+    <section className={classes.section}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        justifyContent="center"
+        alignItems="center"
+        maxWidth="1200px"
+        width="100%"
+        margin="0 auto"
+      ></Box>
+    </section>
+  );
 };
 
 export default FaqSection;
