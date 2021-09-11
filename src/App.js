4cd3484d1974205175d312053dee857c097dd240
Maciej Rapacki <mac.rapacki@gmail.com>
2021-09-11 11:43:39 +0100

    fix: remove unused code and comments

--- src/App.js
+++ src/App.js
@@ -2,30 +2,16 @@ import React from "react";
 
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./utilis/theme";
-import { makeStyles } from "@material-ui/core";
 
 import NavBar from "./components/organisms/navBar/NavBar";
 import ServerPage from "./pages/ServerPage";
 
-import BgImage from "./images-svg/bg-white.png";
-
-const useStyles = makeStyles({
-  main: {
-    // display: "flex",
-    // flexDirection: "row",
-    // justifyContent: "center",
-    // alignItems: "center",
-    // flexWrap: "wrap",
-  },
-});
-
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <NavBar />
-        <main className={classes.main}>
+        <main>
           <ServerPage />
         </main>
       </ThemeProvider>
